2c2d578c52279fa6efea4737b99222a1e44ea0d0
alex-dean23 <61474538+alex-dean23@users.noreply.github.com>
2020-06-07 03:55:59 -0300

    Add files via upload

---
+++ src/ui/Appointments.java
@@ -0,0 +1,114 @@
+package ui;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import javax.swing.ImageIcon;
+
+public class Appointments {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Appointments window = new Appointments();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Appointments() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 550, 400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.DARK_GRAY);
+		panel.setBounds(0, 0, 133, 361);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Dashboard");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(10, 73, 113, 23);
+		panel.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Appointment");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_1.setBounds(10, 107, 113, 23);
+		panel.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Project");
+		btnNewButton_2.setBounds(10, 141, 113, 23);
+		panel.add(btnNewButton_2);
+		
+		JTextField textField = new JTextField();
+		textField.setBounds(241, 70, 189, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JTextField textField_2 = new JTextField();
+		textField_2.setBounds(241, 131, 189, 42);
+		frame.getContentPane().add(textField_2);
+		textField_2.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Project Name");
+		lblNewLabel.setForeground(Color.WHITE);
+		lblNewLabel.setBounds(241, 50, 83, 14);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Project Description");
+		lblNewLabel_1.setForeground(Color.WHITE);
+		lblNewLabel_1.setBounds(243, 112, 107, 14);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		JButton btnNewButton_3 = new JButton("SAVE");
+		btnNewButton_3.setBackground(Color.GREEN);
+		btnNewButton_3.setBounds(241, 196, 83, 23);
+		frame.getContentPane().add(btnNewButton_3);
+		
+		JButton btnNewButton_4 = new JButton("X");
+		btnNewButton_4.setBackground(Color.RED);
+		btnNewButton_4.setBounds(391, 196, 39, 23);
+		frame.getContentPane().add(btnNewButton_4);
+		
+		JLabel lblNewLabel_2 = new JLabel("New label");
+		lblNewLabel_2.setIcon(new ImageIcon(Appointments.class.getResource("/ui/images/pexels-photo-1629212.jpeg")));
+		lblNewLabel_2.setBounds(132, 0, 402, 361);
+		frame.getContentPane().add(lblNewLabel_2);
+	}
+
+}
---
+++ src/ui/Login.java
@@ -0,0 +1,84 @@
+package ui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTextPane;
+import javax.swing.ImageIcon;
+
+public class Login {
+
+	private JFrame frame;
+	private JTextField textField;
+	private JPasswordField passwordField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login window = new Login();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Login() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setForeground(Color.DARK_GRAY);
+		frame.setBounds(100, 100, 550, 400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.DARK_GRAY);
+		panel.setBounds(0, 0, 226, 361);
+		frame.getContentPane().add(panel);
+		
+		JLabel lblNewLabel_2 = new JLabel("New label");
+		panel.add(lblNewLabel_2);
+		lblNewLabel_2.setIcon(new ImageIcon(Login.class.getResource("/ui/images/PMS loginPage(271,361).png")));
+		
+		textField = new JTextField();
+		textField.setBounds(310, 77, 161, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(310, 127, 161, 20);
+		frame.getContentPane().add(passwordField);
+		
+		JLabel lblNewLabel = new JLabel("USERNAME");
+		lblNewLabel.setBounds(312, 58, 63, 14);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("PASSWORD");
+		lblNewLabel_1.setBounds(310, 108, 65, 14);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		JButton btnNewButton = new JButton("LOGIN");
+		btnNewButton.setBounds(310, 182, 161, 23);
+		frame.getContentPane().add(btnNewButton);
+	}
+}
